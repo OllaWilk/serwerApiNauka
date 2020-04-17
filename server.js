a300c6078c3b4810b802debc91d4a062bca29d46
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-04-17 13:35:56 +0200

    add middleware and install corse

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+/*middleware*/
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use(cors());
 
 app.listen(8000, () => {
   console.log('Server is running on port 8000');
