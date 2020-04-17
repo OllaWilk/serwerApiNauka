3aab5eab99e95fda6c70e05c06878362a87d1bb0
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-04-17 13:39:55 +0200

    add db

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ const cors = require('cors');
 
 const app = express();
 
+/*database */
+
+const db = [
+    { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
+    { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
+  ];
+
 /*middleware*/
+
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
